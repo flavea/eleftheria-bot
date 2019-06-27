509a2bb52fbecc1e7e885dcdd8aa761ed90897b0
Ilma Arifiany <ilma@fabelio.com>
2019-06-28 00:35:19 +0700

    change limit

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ client.on('message', message => {
             else if (isNaN(parseInt(args[0]))) return message.reply('Parameter harus angka oi.');
             else limit = parseInt(args[0])
 
-            if (limit > 25) return message.reply('Limit maksimal 25!')
+            if (limit > 20) return message.reply('Limit maksimal 20!')
 
             message.channel.send('Tunggu sebentar ya, sayang, datanya lagi diambil, nih. Kalau gak muncul-muncul, aku lagi halu.')
             eleftheria.getTopCampers(client, message, limit)
