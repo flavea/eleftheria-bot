159474ac19a03e2653c65db86200e80460b4a5f7
Ilma Arifiany <ilma@fabelio.com>
2019-10-17 22:32:38 +0700

    fmk

--- commands/fmk2.js
+++ commands/fmk2.js
@@ -5,12 +5,12 @@ module.exports = {
 	description: 'Pilihan Nicollo',
 	execute(message, client) {
         let args = message.content.split(/ +/)
-        args.shift()
+		args.shift()
 		if (args.length != 3) return message.reply('Masukan 3 nama.')
 		else {
 			for (let i = args.length - 1; i > 0; i--) {
-				const j = Math.floor(Math.random() * (i + 1))
-				[args[i], args[j]] = [args[j], args[i]]
+				const j = Math.floor(Math.random() * (i + 1));
+				[args[i], args[j]] = [args[j], args[i]];
 			}
 
 			let string = `I will F: ${args[0]}, M: ${args[1]}, K: ${args[2]}\n`
